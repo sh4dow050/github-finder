cfc608f787e21bf8e7bff1df9159dbee17e629e9
Naveen <naveensalgaonker@gmail.com>
2021-01-09 13:24:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return <div className='App'>Hello its me juvinaei</div>;
+	}
+}
+
+export default App;
